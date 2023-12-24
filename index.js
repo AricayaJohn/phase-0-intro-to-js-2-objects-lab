bb291ea3cf9b815325c59a4b8d91cd92578f5984
John Aricaya <jaricaya@gmail.com>
2023-12-23 18:59:53 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "John",
+    streetAddress : "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj [key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    //clone 
+    const newEmployee = {...employee};
+    //delete
+    delete newEmployee[key];
+    //return new employee without key and without destroying original 
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
